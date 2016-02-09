834485491c086dfc9acff66403b5106400573da6
Johan Magnusson <jcmmagnusson@gmail.com>
2016-02-09 20:46:19 +0100

    Created a method for printing submission file

--- PaintingTestProblem/src/FileManager.java
+++ PaintingTestProblem/src/FileManager.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * @author Johan Magnusson
  *         Date: 2016-02-09
@@ -5,5 +9,24 @@
 public class FileManager {
     public FileManager() {
 
+    }
+
+    public static void createSubmissionFile(List<Command> commands) {
+        int numberOfCommands = commands.size();
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(numberOfCommands).append("\n");
+
+        for (Command command : commands) {
+            stringBuilder.append(command.getCommandOutput()).append("\n");
+        }
+
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter("submission_file.txt");
+            fileWriter.write(stringBuilder.toString());
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
